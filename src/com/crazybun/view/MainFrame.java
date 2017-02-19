fe73a7562365fa8bb36c7fd0038791e095f554be
CrazyBunQnQ <baobao222222@qq.com>
2017-02-19 22:15:48 +0800

    别紧张，只是改个标题...

--- src/com/crazybun/view/MainFrame.java
+++ src/com/crazybun/view/MainFrame.java
@@ -20,7 +20,7 @@ public class MainFrame {
 		// 创建窗口对象
 		JFrame frame = new JFrame();
 		frame.setSize(Setting.FRAME_WIDTH, Setting.FRAME_HEIGHT);
-		frame.setTitle("包子打飞机");
+		frame.setTitle("打飞机");
 		frame.setIconImage(new ImageIcon("images/ico.png").getImage());
 		// 居中显示窗口
 		frame.setLocation((ScreenUtil.getScreenX() - Setting.FRAME_WIDTH) / 2, (ScreenUtil.getScreenY() - Setting.FRAME_HEIGHT) / 2);
